687b4b7a63d82f2db23640e2153eaaada0aa8a20
Christina McNerney <cmcnern1@jhu.edu>
2018-08-28 13:24:40 -0400

    adding exercise 4

---
+++ day2-lunch/day2-exercise-4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    f = open( sys.argv[1] )
+else:
+    f = sys.stdin
+
+count = 0
+
+for line in f:
+    if line.startswith("@"):
+        continue
+    fields = line.split("\t")
+    if fields[2] != "*":
+        count += 1
+        if count < 11:
+            print(fields[2])
+    else:
+        break
